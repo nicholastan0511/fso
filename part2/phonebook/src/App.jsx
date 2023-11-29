e339e83babadabf38327de01458095f2fea1682e
Nicholas Tan <swaggychoz@gmail.com>
2023-11-29 16:39:13 +0800

    commit exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
+import axios from 'axios'
 
 const App = () => {
+
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '0931267843', id: 1},
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
+  
   const [newName, setNewName] = useState('Bob')
   const [newNum, setNewNum] = useState('')
-
   const [filter, setFilter] = useState('')
 
   const addNewPerson = (event) => {
@@ -72,6 +80,5 @@ const App = () => {
     </div>
   )
 }
-
 
 export default App
