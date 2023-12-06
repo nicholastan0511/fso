fcbad92199026d87a2543f9f86d4f4df1e25c0e1
nicholastan0511 <nicholastan0511@gmail.com>
2023-12-06 16:28:30 +0800

    commit exercises 3.7~3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+
+app.use(morgan(function (tokens, req, res) {
+  console.log(req.body, typeof req.body)
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
 
 const generateId = () => {
   return Math.floor(Math.random() * (10000 - 1 + 1 ) + 1)
@@ -60,9 +73,6 @@ app.delete('/api/persons/:id', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
-
-  console.log(body)
-
   if (!body.name) {
     return res.status(400).json({
       error: 'name missing'
@@ -83,7 +93,6 @@ app.post('/api/persons', (req, res) => {
     id: generateId(),
   }
 
-  console.log(newPerson)
 
   persons = persons.concat(newPerson)
   res.json(newPerson)
