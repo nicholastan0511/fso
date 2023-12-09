10a79831bf5e6a9da3992024b6ad97c626e7e7c3
Nicholas Tan <nicholastan0511@gmail.com>
2023-12-09 21:46:41 +0800

    commit exercise 3.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,10 +8,10 @@ import personTool from './services/persons.js'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '0931267843', id: 1},
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    // { name: 'Arto Hellas', number: '0931267843', id: 1},
+    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
 
   useEffect(() => {
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 app.use(morgan(function (tokens, req, res) {
   console.log(req.body, typeof req.body)
