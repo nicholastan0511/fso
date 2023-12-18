ff2e0974c258ebde93346762541ac1adbccc7561
nicholastan <nicholastan0511@gmail.com>
2023-12-18 19:21:07 +0800

    commit exercises 4.1 - 4.2

---
+++ part4/blog/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogsRouter')
+
+mongoose.set('strictQuery', false)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
