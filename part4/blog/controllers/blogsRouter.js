cd46b67683c4c6ba91e14531703fc387468f89b8
Nicholas Tan <swaggychoz@gmail.com>
2023-12-21 20:58:35 +0800

    commit exercise 4.19

--- part4/blog/controllers/blogsRouter.js
+++ part4/blog/controllers/blogsRouter.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 
 const Blog = require('../models/blog')
 const User = require('../models/user')
@@ -8,8 +9,24 @@ blogsRouter.get('', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFrom = req => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('', async (request, response) => {
   const body = request.body
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
@@ -20,12 +37,8 @@ blogsRouter.post('', async (request, response) => {
   })
 
   const savedBlog = await blog.save()
-
-  const user = await User.findById(body.user)
   user.blogs = user.blogs.concat(savedBlog._id)
-
   await user.save()
-
   response.status(201).json(savedBlog)
 })
 
